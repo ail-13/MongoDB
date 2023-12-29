4ff5d6eac5ea4f42b40a51a14d86f674c89cc0c0
ail-13 <ail-13@mail.ru>
2023-12-29 12:38:00 +0300

    fix: prod

--- lesson-9/terraform/prod/outputs.tf
+++ lesson-9/terraform/prod/outputs.tf
@@ -8,15 +8,17 @@ output "inventory" {
       hostvars : merge(
         {
           for instance in module.db.instances : instance.name => {
-            ansible_host = instance.ip
-            vm_disk      = instance.disk
-            vm_username  = var.username
+            ansible_host  = instance.ip
+            vm_disk       = instance.disk
+            monitoring_ip = module.monitoring.instances[0].ip
+            vm_username   = var.username
           }
         },
         {
           for instance in module.app.instances : instance.name => {
             ansible_host = instance.ip
             mongodb_ip   = module.db.instances[*].ip
+            monitoring_ip = module.monitoring.instances[0].ip
             vm_username  = var.username
           }
         },
@@ -24,6 +26,7 @@ output "inventory" {
           for instance in module.monitoring.instances : instance.name => {
             ansible_host = instance.ip
             mongodb_ip   = module.db.instances[*].ip
+            vm_disk      = instance.disk
             vm_username  = var.username
           }
         }
